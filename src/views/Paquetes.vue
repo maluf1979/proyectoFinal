d9575f1e517f975fa64529c0450d026e44d5a456
HernandezEloy <hernandez.eloy@gmail.com>
2023-05-07 11:09:27 -0300

    Creacion de boton desde Paquetes para volver a Home.

--- src/views/Paquetes.vue
+++ src/views/Paquetes.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <h2>Paquetes</h2>
+    <button @click="irHome">Ir a Home</button>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    irHome() {
+      this.$router.push("/HomeView");
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>

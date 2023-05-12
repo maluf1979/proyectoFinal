225069892138f245873f11eb79a37473592dd2e8
HernandezEloy <hernandez.eloy@gmail.com>
2023-05-11 21:56:30 -0300

    Login Clase.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import { IonApp, IonHeader } from "@ionic/vue";
+import { storeToRefs } from "pinia";
+import { useLoginStore } from "./stores/login";
 
 export default {
   components: { IonApp, IonHeader },
+  setup() {
+    const store = useLoginStore();
+    const { isLogin, user } = storeToRefs(store);
+    const { hasPermissions } = store;
+    return { isLogin, user, hasPermissions };
+  },
 };
 </script>
 
 <template>
   <ion-app>
-    <ion-header style="background-color: white;">
-      <RouterLink to="/home">Home|</RouterLink>
+    <ion-header style="background-color: white">
+      <RouterLink to="/">Home|</RouterLink>
       <RouterLink to="/about">About|</RouterLink>
-      <RouterLink to="/paquetes">Paquetes</RouterLink>
+      <RouterLink v-if="isLogin" to="/paquetes">Paquetes|</RouterLink>
+      <RouterLink v-if="!isLogin" to="/login">Login|</RouterLink>
+      <RouterLink v-if="isLogin" to="/logout">Logout|</RouterLink>
+      Usuario: {{ user.email }}
     </ion-header>
     <RouterView />
   </ion-app>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 //Aca van las rutas de la aplicacion, las cuales se cargan en APP.vue
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
 import HomeView from "../views/HomeView.vue";
-import Paquetes from "../views/Paquetes.vue";//Importo Paquetes.vue
+import Paquetes from "../views/Paquetes.vue"; //Importo Paquetes.vue
 import NotFound from "../views/NotFound.vue";
+import LoginView from '../views/LoginView.vue'
+import LogoutView from '../views/LogoutView.vue'
+import { useLoginStore } from '../stores/login'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/home",
+      path: "/",
       name: "home",
       component: HomeView,
     },
@@ -20,17 +23,38 @@ const router = createRouter({
       // que se carga de forma diferida (lazy-loaded) cuando se visita la ruta.
       component: () => import("../views/AboutView.vue"),
     },
-    { //Cargo la Ruta.
+    {
+      //Cargo la Ruta.
       path: "/paquetes",
       name: "paquetes",
       component: Paquetes,
+      meta: { RequireAuth: true },
     },
     {
-      path:'/:pathMatch(.*)*',
-      name:"NotFound",
-      component: NotFound
-      },
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: NotFound,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: LogoutView,
+    },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const store = useLoginStore();
+  if (to.matched.some(r => r.meta.RequireAuth) && !store.isLogin) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 
 export default router;
